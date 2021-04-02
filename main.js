4d71329fe3843a43cf4293f0db4e6b97e9a3def1
Codermallar23451 <77740669+Codermallar23451@users.noreply.github.com>
2021-04-02 18:31:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function showmenu() {
+    var list_of_menu=["Chicken Tandoori Pizza","Veg Supreme Pizza","Paneer Tikka Pizza","Deleux Veggie Pizza","Veg Extravaganza Pizza"];
+    list_of_menu.sort();
+    console.log(list_of_menu);
+    document.getElementById("listofmenulabel");  
+}
